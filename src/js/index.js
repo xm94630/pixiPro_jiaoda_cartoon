79e69f461744b7769813a5a06baf97fc8d0b2637
xm94630 <xm94630@126.com>
2018-09-24 12:15:07 +0800

    [小发现]同一个mc实例只能被添加到一个容器中

--- src/js/index.js
+++ src/js/index.js
@@ -174,6 +174,7 @@ function getAllMaterial(res){
       return txt;
     },
 
+
   }
 }
 
@@ -307,3 +308,5 @@ console.log('=======>')
 //这两个等效
 //var bgImg = PIXI.Sprite.fromImage("background")
 //var bgImg = new PIXI.Sprite(res.background.texture)
+
+//同一个mc实例只能被添加到一个容器中,,,
